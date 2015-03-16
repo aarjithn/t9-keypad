cdf8698fd181b8dc77b265eab7fc48efcca4d4a2
aarjithn <aarjithn@gmail.com>
2015-03-16 21:51:11 +0530

    Prettifying

---
+++ js/phone.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+    $("#phone").find("button").mouseup(function(event){
+        var button_pressed = $(event.currentTarget).data("value");
+        $("#result").val(t9($("#result").val(),button_pressed));
+    })
+})
+function t9(text,button_pressed){
+    this.keypad=[[" ","0"],[".",",","!","1"],["a","b","c","2"],["d","e","f","3"],["g","h","i","4"],["j","k","l","5"],["m","n","o","6"],["p","q","r","s","7"],["t","u","v","8"],["w","x","y","z","9"]];
+    var pos;
+    if(["*","#"].indexOf(button_pressed) > -1) {
+        text += button_pressed;
+    }
+    else{
+        pos = keypad[button_pressed].indexOf(text.charAt(text.length-1));
+        if((pos > -1) && (this.wait == true)) {
+            clearTimeout(this.timer);
+            text=text.slice(0,text.length-1) + keypad[button_pressed][(pos+1)%keypad[button_pressed].length];
+            this.timer = setTimeout(function(){this.wait = false;},1000);
+            this.wait = true;
+        }
+        else{
+            text += keypad[button_pressed][0];
+            this.timer = setTimeout(function(){this.wait = false;},1000);
+            this.wait = true;
+        }
+    }
+    return text;
+}
