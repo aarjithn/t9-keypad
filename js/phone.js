08e8234ab48e5395dc63afc4444aa3da1c344f4d
aarjithn <aarjithn@gmail.com>
2015-03-16 22:00:56 +0530

    Clear button

--- js/phone.js
+++ js/phone.js
@@ -2,7 +2,10 @@ $(document).ready(function(){
     $("#phone").find("button").mouseup(function(event){
         var button_pressed = $(event.currentTarget).data("value");
         $("#result").val(t9($("#result").val(),button_pressed));
-    })
+    });
+    $(".btn-right").click(function() {
+    	$("#result").val(($("#result").val().slice(0,-1)));
+    });
 })
 function t9(text,button_pressed){
     this.keypad=[[" ","0"],[".",",","!","1"],["a","b","c","2"],["d","e","f","3"],["g","h","i","4"],["j","k","l","5"],["m","n","o","6"],["p","q","r","s","7"],["t","u","v","8"],["w","x","y","z","9"]];
